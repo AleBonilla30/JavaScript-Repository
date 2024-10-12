c2994d5aad2f33320f83f9e347220be54a7a806e
Alejandra <ale.bonilla309@gmail.com>
2024-10-12 13:12:23 +0200

    up

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,11 @@
+console.log("Hola JavaScript");
+console.log('Hola JavaScript');
+console.log(`Hola JavaScript`);
+
+console.log(5);
+console.log("5");
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 ** 2);//este es el exponente con dos asteriscos
---
+++ Basic/01-variables.js
@@ -0,0 +1,12 @@
+//hay tres formas de crear una variable
+//var = fue la forma original de crear una variable puede ser accesible por cualquier lado aunque no se le haya asignado un valor
+var helloWorld = "¡Hello, JavaScript!";
+console.log(helloWorld);
+
+//let solo son accesibles dentro de un bloque
+let helloWorld2 = "¡Hola, JavaScript 2!";
+console.log(helloWorld2);
+
+//const no se puede reasignar el valor
+const helloWorld3 = "¡Hola, JavaScript 3!";
+console.log(helloWorld3);
