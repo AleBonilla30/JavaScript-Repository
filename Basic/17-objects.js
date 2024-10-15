2d02f0aaa2a5ebb371903c9816997fed724c14e5
AleBonilla30 <ale.bonilla309@gmail.com>
2024-10-15 17:52:14 +0200

    objects

---
+++ Basic/17-objects.js
@@ -0,0 +1,18 @@
+//objects todo lo que no es un dato primitivo es un objeto 
+
+//sintanxis
+
+let person = {
+    name: "Ale",
+    age: 33,
+    alias: "aleBo"
+}
+
+//acceso a las propiedades
+
+//notacion llamada por punto 
+console.log(person.name);
+
+//notacion de corchetes
+console.log(person["name"]);
+
